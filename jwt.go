bfc21a27878da2662ee89c6c9457a4c174e49d84
PiccoloMondoC <119958916+PiccoloMondoC@users.noreply.github.com>
2023-05-23 00:45:51 -0500

    Add files via upload

---
+++ jwt.go
@@ -0,0 +1,87 @@
+// sky-common/jwt/jwt.go
+package jwt
+
+import (
+	"errors"
+	"time"
+
+	golangjwt "github.com/golang-jwt/jwt/v4"
+)
+
+// A custom type for the context key to avoid potential collisions with other context keys.
+type contextKey string
+
+const (
+	// ClaimsKey is the context key for storing JWT claims in the request context.
+	ClaimsKey contextKey = "claims"
+	// TokenKey is the context key for storing the JWT token in the request context.
+	TokenKey contextKey = "token"
+)
+
+type JWTConfig struct {
+	VerifyIssuer    bool
+	Issuer          string
+	SigningMethod   golangjwt.SigningMethod
+	SigningKey      []byte
+	ValidationDelay time.Duration
+	Leeway          int64
+}
+
+// Add a custom Claims structure to include internal role, external roles, and permissions
+type Claims struct {
+	golangjwt.RegisteredClaims
+	Email       string   `json:"email"`
+	Roles       []string `json:"roles"`
+	Permissions []string `json:"permissions"`
+	Type        string   `json:"type"`
+}
+
+// IsValidJWT is a custom validator to check if the provided string is a valid JWT token.
+func IsValidJWT(value interface{}) error {
+	str, ok := value.(string)
+	if !ok {
+		return errors.New("invalid JWT token")
+	}
+
+	parser := golangjwt.Parser{}
+	_, _, err := parser.ParseUnverified(str, &Claims{})
+	if err != nil {
+		return errors.New("invalid JWT token")
+	}
+
+	return nil
+}
+
+// GetSubject extracts the subject (user ID) from a JWT token.
+func GetSubject(tokenString string) (string, error) {
+	parser := golangjwt.Parser{}
+	token, _, err := parser.ParseUnverified(tokenString, &Claims{})
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok {
+		return "", errors.New("invalid JWT claims")
+	}
+
+	return claims.Subject, nil
+}
+
+// Parse function validates and extracts the JWT claims.
+func Parse(tokenString string, signingKey []byte) (*Claims, error) {
+	token, err := golangjwt.ParseWithClaims(tokenString, &Claims{}, func(token *golangjwt.Token) (interface{}, error) {
+		return signingKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok {
+		return nil, errors.New("invalid JWT claims")
+	}
+
+	return claims, nil
+}
---
+++ logtypes.go
@@ -0,0 +1,32 @@
+// sky-common/logtypes/logtypes.go
+package logtypes
+
+type LogEntry struct {
+	Service     string                 `json:"service"`
+	LogName     string                 `json:"logName"`
+	Resource    Resource               `json:"resource"`
+	Timestamp   string                 `json:"timestamp"`
+	Level       string                 `json:"level"`
+	Message     string                 `json:"message"`
+	InsertId    string                 `json:"insertId,omitempty"`
+	HttpRequest HttpRequest            `json:"httpRequest,omitempty"`
+	TextPayload string                 `json:"textPayload,omitempty"`
+	JsonPayload map[string]interface{} `json:"jsonPayload,omitempty"`
+}
+
+type Resource struct {
+	Type   string            `json:"type"`
+	Labels map[string]string `json:"labels"`
+}
+
+type HttpRequest struct {
+	RequestMethod string `json:"requestMethod"`
+	RequestUrl    string `json:"requestUrl"`
+	Status        int    `json:"status"`
+	UserAgent     string `json:"userAgent"`
+	RemoteIp      string `json:"remoteIp"`
+}
+
+type LogData struct {
+	Log LogEntry `json:"log"`
+}
